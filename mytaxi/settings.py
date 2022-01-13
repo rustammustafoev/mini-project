7a509a3328b779cf9cdfc62ec1a11f73026d95c7
Rustam Mustafoev <rustam.mustafoev01@gmail.com>
2022-01-13 10:48:27 +0500

    mysql connected, models are create

--- mytaxi/settings.py
+++ mytaxi/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mytaxi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'orderdb',
+        'USER': 'root',
+        'PASSWORD': 'russdevc?!2001',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- order/models.py
+++ order/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=13)
+    car_number = models.CharField(max_length=13, null=True)
+    car_type = models.CharField(max_length=25, null=True)
+    joined = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=13)
+
+
+class Order(models.Model):
+    ORDER_STATUSES = (
+        (1, 'Created'),
+        (2, 'Cancelled'),
+        (3, 'Accepted'),
+        (4, 'Finished'),
+    )
+    driver = models.ForeignKey(Driver, on_delete=models.DO_NOTHING)
+    client = models.ForeignKey(Client, on_delete=models.DO_NOTHING)
+    order_status = models.PositiveSmallIntegerField(choices=ORDER_STATUSES, default=1)
+    order_start_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['order_start_time']
